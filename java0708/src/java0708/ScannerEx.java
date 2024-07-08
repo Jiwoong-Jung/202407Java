c8abbe306eda57812ec35997b70e83e593dc45fd
jiwoong-jung <jjw981119@naver.com>
2024-07-08 16:24:42 +0900

    Scanner 사용 예제

---
+++ java0708/src/java0708/ScannerEx.java
@@ -0,0 +1,17 @@
+package java0708;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print(">>");
+		String str = scan.nextLine();
+		
+		System.out.println(str);
+
+	}
+
+}
