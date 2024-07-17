99c7a454610522f3ea237ce55e41dc069bf7d678
jiwoong-jung <jjw981119@naver.com>
2024-07-17 14:38:37 +0900

    0717 오전

--- java0717/src/swing/MyFrame.java
+++ java0717/src/swing/MyFrame.java
@@ -1,8 +1,14 @@
 package swing;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+
 
 public class MyFrame {
 	public static void main(String[] ar) {
@@ -21,5 +27,55 @@ class MouseListenerEx extends JFrame {
 		this.setSize(300, 400);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		jp.addMouseListener(new MyMouseListener());
+	}
+	
+	class MyMouseListener implements MouseListener, MouseMotionListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			la.setText(e.getX() + "," + e.getY());
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
+
+
