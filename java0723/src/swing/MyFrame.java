69da7e1a8dec2d11d44a2f73945dec1d9bdc2f07
jiwoong-jung <jjw981119@naver.com>
2024-07-23 15:43:42 +0900

    0723 오후

--- java0723/src/swing/MyFrame.java
+++ java0723/src/swing/MyFrame.java
@@ -25,6 +25,8 @@ public class MyFrame extends JFrame {
 		this.setVisible(true);
 		
 		//여기에 Thread를 객체 생성하여 start()메소드를 호출하세요.
+		Thread thread = new Thread(field);
+		thread.start();
 	}
 	
 	private class Field extends JPanel implements Runnable {
@@ -38,10 +40,13 @@ public class MyFrame extends JFrame {
 		@Override
 		public void run() {
 			while (true) {
-				String[] path = {"walk1.gif", "walk2.gif"};
+				//달리는 동영상으로 바꿔 보세요.
+//				String[] path = {"walk1.gif", "walk2.gif"};
+//				String[] path = {"stand.gif"};
+				String[] path = {"run1.gif", "run2.gif", "run3.gif"};
 				for (int i = 0; i < path.length; i++) {
 					lbl.setIcon(new ImageIcon("./src/" + path[i]));
-					System.out.println(i);
+//					System.out.println(i);
 					try {
 						Thread.sleep(300);  // 0.3초
 					} catch (InterruptedException e) {}
