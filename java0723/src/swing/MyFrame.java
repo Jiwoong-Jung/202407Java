3286769e2bfde600508887dfff9365309d8092d1
jiwoong-jung <jjw981119@naver.com>
2024-07-23 15:35:34 +0900

    0723 오후

--- java0723/src/swing/MyFrame.java
+++ java0723/src/swing/MyFrame.java
@@ -23,15 +23,31 @@ public class MyFrame extends JFrame {
 		this.add(control, BorderLayout.SOUTH);
 		this.setBounds(1200, 100, 500, 500);
 		this.setVisible(true);
+		
+		//여기에 Thread를 객체 생성하여 start()메소드를 호출하세요.
 	}
 	
-	private class Field extends JPanel {
+	private class Field extends JPanel implements Runnable {
 		JLabel lbl;
 		Field() {
 			this.setLayout(new BorderLayout());
 			lbl = new JLabel();
 			lbl.setIcon(new ImageIcon("./src/stand.gif"));
 			this.add(lbl, BorderLayout.CENTER);
+		}
+		@Override
+		public void run() {
+			while (true) {
+				String[] path = {"walk1.gif", "walk2.gif"};
+				for (int i = 0; i < path.length; i++) {
+					lbl.setIcon(new ImageIcon("./src/" + path[i]));
+					System.out.println(i);
+					try {
+						Thread.sleep(300);  // 0.3초
+					} catch (InterruptedException e) {}
+				}
+			}
+			
 		}
 	}
 	
