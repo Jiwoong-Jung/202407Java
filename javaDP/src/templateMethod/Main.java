9b29d8c5408af7d6fbfa0c7050438f5c0bce084e
jiwoong-jung <jjw981119@naver.com>
2024-07-24 11:19:29 +0900

    0724 오전

--- javaDP/src/templateMethod/Main.java
+++ javaDP/src/templateMethod/Main.java
@@ -6,6 +6,12 @@ public class Main {
 		// 'H'를 가진 CharDisplay 인스턴스를 하나 만든다 
         AbstractDisplay d1 = new CharDisplay('H');
         d1.display();
+        
+        AbstractDisplay d2 = new StringDisplay("Hello!");
+        d2.display();
+        
+        AbstractDisplay d3 = new StringDisplay("안녕!");
+        d3.display();
 	}
 
 }
