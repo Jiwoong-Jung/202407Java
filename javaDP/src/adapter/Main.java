35b5714b359e2b86a906337ca3da36ad0a9c6680
jiwoong-jung <jjw981119@naver.com>
2024-07-24 10:52:16 +0900

    0724 오전

---
+++ javaDP/src/adapter/Main.java
@@ -0,0 +1,12 @@
+package adapter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+
+	}
+
+}
