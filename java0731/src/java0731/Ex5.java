c77845393513228d81e1c55c06c8e543aac1d1d5
jiwoong-jung <jjw981119@naver.com>
2024-07-31 10:39:03 +0900

    0731 오전

---
+++ java0731/src/java0731/Ex5.java
@@ -0,0 +1,27 @@
+package java0731;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+		List<Member> list = Arrays.asList(
+				new Member("홍길동", Member.MALE, 30),
+				new Member("김길동", Member.FEMALE, 20),
+				new Member("박길동", Member.FEMALE, 45),
+				new Member("최길동", Member.MALE, 27)
+				);
+		//전체 평균 나이
+		double avg = list.stream().mapToInt(a->a.getAge()).average().orElse(0);
+		System.out.println(avg);
+		//남자 평균 나이
+		double avg1 = list.stream()
+				.filter(x->x.getSex() == Member.MALE)   // 남자만 필터링
+				.mapToInt(a->a.getAge())
+				.average().orElse(0);
+		System.out.println(avg1);
+
+	}
+
+}
