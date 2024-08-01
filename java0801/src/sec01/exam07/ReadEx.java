da3ab1d8f2e9953d1385dadc7f0c6e7e9bc0dfdb
jiwoong-jung <jjw981119@naver.com>
2024-08-01 10:38:06 +0900

    0801오전

--- java0801/src/sec01/exam07/ReadEx.java
+++ java0801/src/sec01/exam07/ReadEx.java
@@ -7,15 +7,15 @@ import java.io.IOException;
 public class ReadEx {
 
 	public static void main(String[] args) throws IOException {
-		FileReader reader = new FileReader("C:/Temp/test7.txt");
+//		FileReader reader = new FileReader("C:/Temp/test7.txt");
 		String line = "";
 		
-		BufferedReader br = new BufferedReader(reader);
+		BufferedReader br = new BufferedReader(new FileReader("C:/Temp/board1.csv"));
 		while ((line = br.readLine()) != null) {
 			System.out.println(line);
 		}
 		
-		reader.close();
+		br.close();
 
 	}
 
