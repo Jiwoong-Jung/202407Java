6ca9302b172b5a287a19b3044d4bc75400e356aa
jiwoong-jung <jjw981119@naver.com>
2024-08-01 11:10:05 +0900

    0801오전

--- web0731/src/main/java/drive/ReadEx.java
+++ web0731/src/main/java/drive/ReadEx.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import board.BoardDAO;
 import board.BoardDTO;
 
 public class ReadEx {
@@ -11,6 +12,7 @@ public class ReadEx {
 	public static void main(String[] args) throws IOException {
 //		FileReader reader = new FileReader("C:/Temp/test7.txt");
 		String line = "";
+		BoardDAO dao = new BoardDAO();
 		
 		BufferedReader br = new BufferedReader(new FileReader("C:/Temp/board1.csv"));
 		while ((line = br.readLine()) != null) {
@@ -19,6 +21,8 @@ public class ReadEx {
             
             BoardDTO dto = new BoardDTO(0, data[0], data[1], data[2], null, 0);
             System.out.println(dto);
+            dao.insertBoard(dto);
+            System.out.println("insert 완료!");
 //            System.out.println(data[0]);
 //            System.out.println(data[1]);
 //            System.out.println(data[2]);
