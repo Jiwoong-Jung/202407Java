52d0cb688f3e453d8c09c08f4bfbdc0539823a6e
jiwoong-jung <jjw981119@naver.com>
2024-08-06 16:33:45 +0900

    0806 오후

---
+++ web0806_mybatis/src/main/java/drive/TestMain.java
@@ -0,0 +1,21 @@
+package drive;
+
+import java.util.List;
+
+import dept.DeptDAO;
+import dept.DeptDTO;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		DeptDAO dao = new DeptDAO();
+		int deptno = dao.searchDeptno("SALES");
+		System.out.println(deptno);
+		List<DeptDTO> list = dao.listDept();
+		for (DeptDTO dto : list) {
+			System.out.println(dto);
+		}
+	}
+
+}
